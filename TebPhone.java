b3181bbbc6e6ef95be20bfd8d5677782b8a7743c
tebit <39907823+tebit@users.noreply.github.com>
2019-02-05 15:44:51 +0200

    Add files via upload

---
+++ TebPhone.java
@@ -0,0 +1,12 @@
+public class TebPhone implements Sim{
+    @Override
+    public void calling(){
+System.out.println("Calling using TebPhone mobile");
+    }
+    @Override
+    public void data(){
+        System.out.println("browsing internet using TebPhone Sim");
+
+    }
+
+}
